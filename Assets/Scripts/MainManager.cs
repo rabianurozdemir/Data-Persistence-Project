863aa1b53cce941cc7560f778a2372f72ceb14cb
Rabia Nur Özdemir <74839961+rabianurozdemir@users.noreply.github.com>
2024-02-28 19:11:14 +0300

    Start Menu added.

--- Assets/Scripts/MainManager.cs
+++ Assets/Scripts/MainManager.cs
@@ -11,6 +11,7 @@ public class MainManager : MonoBehaviour
     public Rigidbody Ball;
 
     public Text ScoreText;
+    public Text BestScoreText;
     public GameObject GameOverText;
     
     private bool m_Started = false;
@@ -18,7 +19,6 @@ public class MainManager : MonoBehaviour
     
     private bool m_GameOver = false;
 
-    
     // Start is called before the first frame update
     void Start()
     {
@@ -36,6 +36,7 @@ public class MainManager : MonoBehaviour
                 brick.onDestroyed.AddListener(AddPoint);
             }
         }
+        
     }
 
     private void Update()
